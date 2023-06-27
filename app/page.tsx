078ccc6f404631fdce40d5429c3fe5d754b20270
victorgboynton <victorgboynton@gmail.com>
2023-06-26 18:55:58 -0600

    Tailwind adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,45 @@
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <Description/>
-      <ProjectList />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Description />
+			<ProjectList />
+		</div>
+	);
 }
 
 function Header() {
-  return (
-    <div>
-      <h1>Victor Boynton</h1>
-      <ul  className = "">
-        <li>Contact</li>
-        <li>About Me</li>
-      </ul>
-    </div>
-  );
+	return (
+		<nav className="flex-between w-full px-4">
+			<div className="flex gap-3">
+				<p>Victor Boynton</p>
+			</div>
+			<div className="flex justify-right">
+				<p>Contact</p>
+				<p>About Me</p>
+			</div>
+		</nav>
+	);
 }
 
 function Description() {
-  return (
-    <p>
-      Hello! My name is Victor Boynton, and I am an aspiring web developer. I
-      have worked for years to gather the skills necessary to enter the tech
-      industry, and I think that now is the right time for me to do just that.
-    </p>
-  );
+	return (
+		<p>
+			Hello! My name is Victor Boynton, and I am an aspiring web developer. I
+			have worked for years to gather the skills necessary to enter the tech
+			industry, and I think that now is the right time for me to do just that.
+		</p>
+	);
 }
 
 function ProjectList() {
-  return <p>Project List</p>;
+	return <p>Project List</p>;
 }
 
 function Project() {
-  return <p>project</p>;
+	return <p>project</p>;
 }
 
 function Footer() {
-  return <p>footer</p>;
+	return <p>footer</p>;
 }
