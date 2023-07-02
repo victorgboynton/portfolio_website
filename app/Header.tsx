9d137d2a0c87552cceab457bf991f9f5d79a7513
victorgboynton <victorgboynton@gmail.com>
2023-07-01 19:07:05 -0600

    Asked to commit

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,9 +4,9 @@ export function Header() {
   return (
     <div className="m-4 mx-auto justify-center text-center max-w-3xl bg-gray-800 rounded border-sky-800 border-2">
       <div className="px-4 justify-center">
-        <div className=" font-bold flex-col text-4xl mb-8 text-slate-100">
+        <h1 className=" flex-col text-4xl mb-8 font-extrabold text-slate-100">
           Lorem ipsum dolor sit amet,
-        </div>
+        </h1>
         <em className="text-green-500 text-5xl font-bold">lorem ipsum</em>
       </div>
     </div>
