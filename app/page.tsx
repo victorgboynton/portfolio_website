f80e4b950fe0cb066fe27210c9c27961cb5550ec
victorgboynton <victorgboynton@gmail.com>
2023-07-08 23:33:36 -0600

    Added photo, changed layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Description } from "./Description";
 import { Header } from "./Header";
 import { ProjectList } from "./ProjectList";
+import Image from "next/image";
+import pictureOfMe from "./pictureOfMe.jpg";
 
 export const projects = [
   {
@@ -72,22 +74,35 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div className="bg-zinc-300 border-t-4 border-slate-900">
+      <div className="bg-gradient-to-b from-gray-900 to-gray-500 border-t-4 border-slate-950">
         <ProjectList />
-        <div className="flex flex-wrap justify-center lg:gap-20 mt-8 pb-10 items-center">
-          <Description />
-          <SkillsList />
+        <div className=" md:grid grid-col-3 place-items-center justify-center  mt-8 pb-10 items-center">
+          <div className="col-span-2 flex flex-col mx-4">
+            <Description2 />
+            <div className="mx-auto">
+              <SkillsList>My Skills:</SkillsList>
+            </div>
+          </div>
+          <div className="flex justify-center md:col-start-3">
+            <Image
+              src="/me.JPG"
+              alt="picture of me"
+              className="border-purple-700 "
+              width={500}
+              height={616}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-function SkillsList() {
+function SkillsList({ children }: { children: any }) {
   return (
     <div className="text-center border-2 bg-purple-800/25 max-w-xl border-purple-700 rounded backdrop-blur">
       <p className="border-b-2 font-bold text-slate-50 border-purple-700 text-2xl bg-purple-900">
-        My skills:
+        {children}
       </p>
       <div className="flex flex-wrap mx-auto">
         <ul className="flex flex-wrap justify-center">
@@ -105,5 +120,20 @@ function Skill({ skillObj }: { skillObj: any }) {
     <span className="font-bold border-2 rounded-2xl px-4 m-1 py-1 bg-purple-400 text-slate-950">
       {skillObj.skill}
     </span>
+  );
+}
+
+function Description2() {
+  return (
+    <div className="grid text-center">
+      <h1 className="text-4xl font-extrabold border-b-4 border-purple-700 mb-6 text-black">
+        A LITTLE ABOUT ME
+      </h1>
+      <p className="text-left text-xl">
+        My name is Victor Boynton, and I am an aspiring web developer. I may not
+        hae much experience, but what I lack in formal training, I make up for
+        in gusto and desire to learn. yada yada yada yada yada yada
+      </p>
+    </div>
   );
 }
